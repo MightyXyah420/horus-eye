2ee86543d085f9b9e1913fdf63561b51915f1e2c
MightyXyah420 <juancnq16@gmail.com>
2021-10-19 16:56:31 -0500

    first route test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemComponent } from './item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/todo', pathMatch:'full'},
+  {path:'todo',component: ItemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  constructor() { }
+  title = 'todo';
+
+  filter:'all' | 'active' | 'done' = 'all';
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
